100fe55250a74eb1dd6c171feb6c72f28b5f22d7
atclarkson <atclarkson@gmail.com>
2019-11-04 11:21:14 -0500

    init commit

---
+++ CS19/CS19.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CS19.ino
+ Created:	11/4/2019 11:08:47 AM
+ Author:	atcla
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
